b378c92dc000f1126983f8231d6d599b3d14c358
UnionIM <dam.rowv@gmail.com>
2022-10-31 12:03:45 +0200

    Search bar with hints menu and redirect to coin-list of found coins

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../content/svg/Logo.svg';
 import magnifier from '../../content/svg/Magnifier-white.svg';
+import useDebounce from '../../hooks/useDebounce';
+import { ISearchedCoin } from '../../models/crypto';
+import List from '../List/List';
+import CryptoController from '../../controllers/crypto.controller';
+import LightItem from '../List/ListItems/LightItem/LightItem';
 
 const Navbar = () => {
     const [isSearchClicked, setIsSearchClicked] = useState<boolean>(false);
     const [searchValue, setSearchValue] = useState<string>('');
+    const [searchedCoins, setSearchedCoins] = useState<ISearchedCoin[]>([]);
+    const nav = useNavigate();
+
+    const debounce = useDebounce(searchValue, 500);
+
+    useEffect(() => {
+        if (debounce) {
+            CryptoController.searchForCrypto(debounce).then((r) => {
+                setSearchedCoins(r.coins);
+            });
+        } else {
+            setSearchedCoins([]);
+        }
+    }, [debounce]);
+
+    const searchHandler = (value: string) => {
+        nav(`coin-list/${value}`);
+    };
 
     return (
         <nav className="navbar">
@@ -22,7 +45,7 @@ const Navbar = () => {
                             </Link>
                         </li>
                         <li className="navbar__item">
-                            <Link to={'/coin'}>
+                            <Link to={'/coin-list'}>
                                 COINS LIST <div />
                             </Link>
                         </li>
@@ -37,6 +60,11 @@ const Navbar = () => {
                                 value={searchValue}
                                 onChange={(e) => {
                                     setSearchValue(e.target.value);
+                                }}
+                                onKeyUp={(e) => {
+                                    if (e.key === 'Enter') {
+                                        searchHandler(searchValue);
+                                    }
                                 }}
                             />
                             <div
@@ -62,6 +90,34 @@ const Navbar = () => {
                             />
                         </div>
                     )}
+                    <div
+                        className="navbar__searched_container"
+                        style={
+                            searchValue &&
+                            isSearchClicked &&
+                            searchedCoins.length > 0
+                                ? { opacity: 1 }
+                                : { opacity: 0 }
+                        }
+                    >
+                        <div className="navbar__searched_list">
+                            <List
+                                items={searchedCoins.filter((coin) => {
+                                    return coin.name
+                                        .toLowerCase()
+                                        .startsWith(searchValue);
+                                })}
+                                renderItem={(item) => (
+                                    <Link
+                                        className="navbar__coin_item"
+                                        to={`coins/${item.id}/overview`}
+                                    >
+                                        <LightItem coin={item} />
+                                    </Link>
+                                )}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
