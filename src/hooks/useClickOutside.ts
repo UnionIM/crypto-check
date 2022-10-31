b4af227c04f76b90dc7bab760f44454a071ed41e
UnionIM <dam.rowv@gmail.com>
2022-10-31 11:55:20 +0200

    Hook for dropdown menu

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+export const useOutsideClick = (onClickOutside: () => void, ref: any) => {
+    useEffect(() => {
+        const handleClickOutside = (event: Event) => {
+            if (
+                ref.current &&
+                !ref.current.contains(event.target as HTMLDivElement)
+            ) {
+                onClickOutside && onClickOutside();
+            }
+        };
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, [onClickOutside]);
+};
