ffe2ad7c78832f4dcd4c90d30aa363443468e2e8
UnionIM <dam.rowv@gmail.com>
2022-10-31 11:53:03 +0200

    Triangle arrow fix

--- src/components/UI/TriangleArrow/TriangleArrow.tsx
+++ src/components/UI/TriangleArrow/TriangleArrow.tsx
@@ -3,10 +3,21 @@ import cls from './TriangleArrow.module.scss';
 
 interface TriangleArrowProps {
     isUp: boolean;
+    color?: string;
 }
 
-const TriangleArrow: FC<TriangleArrowProps> = ({ isUp }) => {
-    return <span className={isUp ? cls.arrow_up : cls.arrow_down} />;
+const TriangleArrow: FC<TriangleArrowProps> = ({
+    isUp,
+    color = isUp ? 'green ' : 'red',
+}) => {
+    return (
+        <span
+            className={isUp ? cls.arrow_up : cls.arrow_down}
+            style={{
+                [isUp ? 'borderBottom' : 'borderTop']: `5px solid ${color}`,
+            }}
+        />
+    );
 };
 
 export default TriangleArrow;
