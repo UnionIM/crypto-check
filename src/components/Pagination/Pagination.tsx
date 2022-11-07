7e616fb1aca20c244f8d619195a3fa2be71e899d
UnionIM <dam.rowv@gmail.com>
2022-11-07 17:00:21 +0200

    Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import cls from './Pagination.module.scss';
+
+interface PaginationProps {
+    total: number;
+    perPage: number;
+}
+
+const Pagination: FC<PaginationProps> = ({ total, perPage }) => {
+    const totalPages = Math.ceil(total / perPage);
+    const { page } = useParams();
+    const nav = useNavigate();
+
+    const handlePageClick = (e: { selected: number }) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        return nav(`/coin-list/${e.selected + 1}`);
+    };
+
+    return (
+        <div className={cls.pagination__container}>
+            {page ? (
+                <ReactPaginate
+                    pageCount={totalPages}
+                    onPageChange={handlePageClick}
+                    containerClassName={cls.pagination}
+                    activeClassName={cls.active}
+                    pageLinkClassName={cls.item}
+                    pageRangeDisplayed={8}
+                    initialPage={parseInt(page) - 1}
+                    nextLabel=">"
+                    previousLabel="<"
+                    previousClassName={cls.item}
+                    nextClassName={cls.item}
+                />
+            ) : (
+                <div>Loading...</div>
+            )}
+        </div>
+    );
+};
+
+export default Pagination;
