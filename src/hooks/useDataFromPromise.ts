638e27165bdf9c22a757f2563d2398838dcc32d9
UnionIM <dam.rowv@gmail.com>
2022-11-07 17:01:13 +0200

    Fetch hook fix

--- src/hooks/useDataFromPromise.ts
+++ src/hooks/useDataFromPromise.ts
@@ -3,18 +3,21 @@ import { useEffect, useState } from 'react';
 export default function useDataFromPromise<T>(
     promise: (...args: any) => Promise<T>,
     params: any[],
-    deps?: any
+    deps?: any[]
 ) {
     const [data, setData] = useState<{ data: null | T; isLoading: boolean }>({
         data: null,
         isLoading: true,
     });
 
-    useEffect(() => {
-        promise.apply(null, params).then((value) => {
-            setData({ data: value, isLoading: false });
-        });
-    }, [deps || null]);
+    useEffect(
+        () => {
+            promise.apply(null, params).then((value) => {
+                setData({ data: value, isLoading: false });
+            });
+        },
+        deps ? [...deps] : []
+    );
 
     return data;
 }
