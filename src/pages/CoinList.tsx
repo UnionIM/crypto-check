2686836bb41a3f7b66ec3573cc097ee6367258d9
UnionIM <dam.rowv@gmail.com>
2022-11-07 17:06:49 +0200

    CoinList pagination and sort

--- src/pages/CoinList.tsx
+++ src/pages/CoinList.tsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cls from '../styles/pagesStyle/CoinList.module.scss';
 import useDataFromPromise from '../hooks/useDataFromPromise';
 import CryptoController from '../controllers/crypto.controller';
 import { useParams } from 'react-router-dom';
 import List from '../components/List/List';
 import ExtendedCoinItem from '../components/List/ListItems/ExtendedCoinItem/ExtendedCoinItem';
+import { IFlatExtendedCoin } from '../models/crypto';
+import ListHeader from '../components/List/ListHeader/ListHeader';
+import Pagination from '../components/Pagination/Pagination';
 
 const CoinList = () => {
     const { page } = useParams();
+    const [coins, setCoins] = useState<IFlatExtendedCoin[]>([]);
+    const [sort, setSort] = useState<{ column: string; isDesc: boolean }>({
+        column: 'market_cap',
+        isDesc: true,
+    });
+
+    const listHeaders = [
+        {
+            title: 'Name',
+            sort: 'name',
+            width: window.innerWidth < 1000 ? 108 : 210,
+        },
+        { title: 'Price', sort: 'price', width: 97 },
+        { title: '24h', sort: 'price24h', width: 92 },
+        { title: '7d', sort: 'price7d', width: 95 },
+        { title: '30d', sort: 'price30d', width: 85 },
+        { title: 'Market cap', sort: 'market_cap', width: 135 },
+        { title: 'MC 24h', sort: 'market_cap_24h', width: 80 },
+        { title: 'FDV', sort: 'fdv', width: 130 },
+        { title: 'MC/FDV', sort: 'mc_fdv', width: 60 },
+    ];
 
     const { data, isLoading } = useDataFromPromise(
         CryptoController.getAllCrypto,
+        [page],
         [page]
     );
+
+    useEffect(() => {
+        if (data?.data) {
+            const temp: IFlatExtendedCoin[] = [];
+            data.data.map((el) => {
+                temp.push(CryptoController.extendedCoinNormalizer(el));
+            });
+            setCoins(temp);
+        }
+    }, [data?.data]);
+
+    useEffect(() => {
+        setCoins(sortArr(coins, sort.column, sort.isDesc));
+    }, [sort]);
+
+    const sortArr = (arr: any[], property: string, isDesc: boolean) => {
+        return [...arr].sort((a, b) => {
+            return (
+                (a[property] < b[property]
+                    ? -1
+                    : a[property] > b[property]
+                    ? 1
+                    : 0) * (isDesc ? -1 : 1)
+            );
+        });
+    };
 
     if (!data) {
         return <div>no data</div>;
@@ -20,23 +71,32 @@ const CoinList = () => {
 
     return (
         <div className={cls.coin_list}>
-            <div className={cls.coin_item}>
-                <div className={cls.coin_item__name_info}>Name</div>
-                <div className={cls.coin_item__price}>Price</div>
-                <div className={cls.coin_item__percentage}>24h</div>
-                <div className={cls.coin_item__percentage}>7d</div>
-                <div className={cls.coin_item__percentage}>30d</div>
-                <div className={cls.coin_item__market_cap}>Market cap</div>
-                <div className={cls.coin_item__percentage}>MC 24h</div>
-                <div className={cls.coin_item__max_market_cap}>FDV</div>
-                <div className={cls.coin_item__part_of_market_cap}>MC/FDV</div>
-            </div>
-            <List
-                items={data}
-                renderItem={(item) => (
-                    <ExtendedCoinItem coin={item} key={item.id} />
-                )}
-            />
+            {isLoading ? (
+                <div>Loading</div>
+            ) : (
+                <div className={cls.coin_list__container}>
+                    <div className={cls.coin_item}>
+                        <ListHeader
+                            listHeaders={listHeaders}
+                            setSort={setSort}
+                        />
+                    </div>
+                    <List
+                        items={coins}
+                        renderItem={(item) => (
+                            <ExtendedCoinItem coin={item} key={item.id} />
+                        )}
+                    />
+                    {data?.headers.total ? (
+                        <Pagination
+                            total={parseInt(data?.headers.total)}
+                            perPage={50}
+                        />
+                    ) : (
+                        <div>Loading</div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
